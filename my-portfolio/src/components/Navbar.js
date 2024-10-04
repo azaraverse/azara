bdd12e4a4fc1095cad7440904e90456c85e2df1b
Azara <yyushahu02@gmail.com>
2024-10-04 23:53:33 +0000

    update navbar to use useState in designing a responsive mobile view app. incorporate a mobile menu that fallsback on useState()

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="bg-white shadow-md fixed w-full z-10 top-0 p-6">
       <div className="container mx-auto flex justify-between items-center">
+
+        {/** Logo */}
         <NavLink to='/' className="text-skin text-lg">
           azara_( )
         </NavLink>
+
+        {/** Desktop NavLinks */}
         <div className="hidden md:flex">
           <NavLink
             to='/portfolio'
@@ -21,10 +28,25 @@ const Navbar = () => {
         </div>
         {/** Hamburger icon for mobile view */}
         <div className="md:hidden">
-          <button className="text-skin">
+          <button className="text-skin focus:outline-none" onClick={() => setIsMenuOpen(!isMenuOpen)}>
             <span>&#9776;</span>
           </button>
         </div>
+      </div>
+
+      {/** Mobile Menu */}
+      <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'} bg-white rounded-lg shadow-lg transition-all duration-300 ease-in-out`}>
+        <NavLink
+          to='/portfolio'
+          /* close Menu on item click using setIsMenuOpen */
+          className="block text-skin py-2 px-6 hover:text-black" onClick={() => setIsMenuOpen(false)}>
+            portfolio_( )
+        </NavLink>
+        <NavLink
+          to='/blog'
+          className="block text-skin py-2 px-6 hover:text-black" onClick={() => setIsMenuOpen(false)}>
+            blog_( )
+        </NavLink>
       </div>
     </nav>
   )
--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="text-center">
-      <h1 className="text-5xl font-extrabold mt-20">
+      <h1 className="text-4xl font-bold mt-20">
         Hello World, I go by Azara. Actual name? Yushahu Yussif!
       </h1>
       <p className="text-xl mt-4">
@@ -12,9 +12,9 @@ const Home = () => {
 
       <div className="mt-8">
         <button className="bg-skin hover:bg-skin-light text-white px-4 py-2 rounded-lg shadow-lg">
-          <Link to='/portfolio'>
+          <NavLink to='/portfolio'>
             View Portfolio
-          </Link>
+          </NavLink>
         </button>
       </div>
     </div>
