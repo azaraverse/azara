fa274301ba7d7d92540ba6573c7c67df4ec82ad2
Azara <yyushahu02@gmail.com>
2024-10-04 19:30:31 +0000

    init-commit: project initialisation. setup project structure and write a small homepage.

---
+++ my-portfolio/src/pages/Home.js
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-5xl font-extrabold mt-10">
+        Hello World, I go by Azara. Actual name? Yushahu Yussif!
+      </h1>
+      <p className="text-xl mt-4">
+        A developer passionate about building anything with code blocks.
+      </p>
+
+      <div className="mt-8">
+        <button className="bg-purple-500 hover:bg-purple-400 text-white px-4 py-2 rounded">
+          View Portfolio
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
